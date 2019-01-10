50aa0aade33ef37b762fd40f64a942d5004244f0
stefan <stefanbogdanovic030@gmail.com>
2019-01-10 10:58:09 +0100

    Handle of get request

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 // Set up express app
 const app = express();
 
+app.get('/api', (req, res) => {
+   console.log('GET request');
+   res.send({name: 'Yoshi'});
+});
+
 // Listen for requests
-app.listen(process.env.port || 4000, () => {
+app.listen(process.env.port || 3000, () => {
     console.log('Now listening for requests');
+
 });
