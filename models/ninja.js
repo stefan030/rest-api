f282e428d85cae4531f180d6da2de2b9ffe83b33
stefan <stefanbogdanovic030@gmail.com>
2019-01-10 13:01:02 +0100

    Schema and Model initialization

---
+++ models/ninja.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create ninja Schema & model
+const NinjaSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name field is required"]
+    },
+    rank: {
+        type: String
+    },
+    available: {
+        type: Boolean,
+        default: false
+    }
+    // add in geo location
+});
+
+// Collection in db - Model
+const Ninja = mongoose.model('ninja', NinjaSchema);
+
+module.exports = Ninja;
